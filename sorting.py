73e44bab5a83fdaa3516d22cae66d4513544a492
Ashok Pandiyan A <ashokpandiyan431998@gmail.com>
2021-10-01 23:54:36 +0530

    Final 01/20/21

--- sorting.py
+++ sorting.py
@@ -1,12 +1,26 @@
-def selectionSort(a):
-    n = len(a)
-    for i in range(n-1):
+# def selectionSort(a):
+#     n = len(a)
+#     for i in range(n-1):
+#         position = i
+#         for j in range(i+1, n):
+#             if a[j] < a[position]:
+#                 position = j
+#         a[i], a[position] = a[position], a[i]
+#     return a
+
+
+# print(selectionSort([4, 7, 2, 3, 10, 1, 5]))
+
+def insertionSort(A):
+    n = len(A)
+    for i in range(1, n):
+        cvalue = A[i]
         position = i
-        for j in range(i+1, n):
-            if a[j] < a[position]:
-                position = j
-        a[i], a[position] = a[position], a[i]
-    return a
+        while position > 0 and A[position-1] > cvalue:
+            A[position] = A[position-1]
+            position = position - 1
+        A[position] = cvalue
+    return A
 
 
-print(selectionSort([4, 7, 2, 3, 10, 1, 5]))
+print(insertionSort([4, 7, 2, 3, 10, 1, 5]))
